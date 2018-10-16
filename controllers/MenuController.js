9ad778bafd69428237fa6995450066afd1ca9e69
David Buser <busdav@outlook.com>
2018-10-16 17:24:22 +0700

    assignment testing: completed

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -54,4 +54,8 @@ module.exports = class MenuController {
   getContactCount() {
     return this.contacts.length;
   }
+
+  remindMe() {
+    console.log('Learning is a life-long pursuit');
+  }
 };
